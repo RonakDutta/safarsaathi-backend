1f138037404a8eadc078f3be5d87564e1bfdca2e
Ronak Dutta <ronakdutta100@gmail.com>
2026-04-09 20:28:15 +0530

    Fixed an issue

--- routes/adminDashboard.js
+++ routes/adminDashboard.js
@@ -13,8 +13,8 @@ const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 // Initiate email
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
--- routes/booking.js
+++ routes/booking.js
@@ -18,8 +18,8 @@ const razorpay = new Razorpay({
 // Create the email sender
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,

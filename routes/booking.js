f23aaf90722139d1a0f3267c74d127b81e9c8bce
Ronak Dutta <ronakdutta100@gmail.com>
2026-04-09 20:38:35 +0530

    Forced Nodemailer to use IPv4 to fix Render block

--- routes/booking.js
+++ routes/booking.js
@@ -20,18 +20,11 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
-
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log("⚠️ Nodemailer Connection Error:", error);
-  } else {
-    console.log("✅ Nodemailer is ready to send messages!");
-  }
 });
 
 const sendBookingMessage = async (
